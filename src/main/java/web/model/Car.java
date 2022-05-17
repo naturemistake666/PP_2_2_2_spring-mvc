da5ba659301778c4ad59e9e26b4a76c37bdc5247
naturemistake666 <65819256+naturemistake666@users.noreply.github.com>
2022-05-17 04:40:40 +0300

    pom update

--- src/main/java/web/model/Car.java
+++ src/main/java/web/model/Car.java
@@ -3,9 +3,7 @@ package web.model;
 public class Car {
     private String model;
     private int series;
-
     private double price;
-
 
     public Car(String model, int series, double price) {
         this.model = model;
